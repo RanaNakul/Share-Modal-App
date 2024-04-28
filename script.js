204f1335ce29d060379f9f5f23e0e5ca548237a6
Nakul Rana <102985207+RanaNakul@users.noreply.github.com>
2024-04-28 20:49:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const Modal = document.querySelector(".modal");
 const Overlay = document.querySelector(".Overlay");
 
 const openModal = () => {
+    console.log("1");
     Modal.classList.add("modalActive");
+    console.log("2");
     Overlay.classList.add("OverlayActive");
+    console.log("3");
 };
 
 const closeModal = () => {
-
+    console.log("4");
     Modal.classList.remove("modalActive");
+    console.log("5");
     Overlay.classList.remove("OverlayActive");
+    console.log("6");
 };
